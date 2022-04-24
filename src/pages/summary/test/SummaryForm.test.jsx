3c3829f0c7b059f749f53a783b869fdcd032d7ab
Carl Olson <colson2275@gmail.com>
2022-04-24 00:18:38 -0400

    add tests for popover on terms and conditions

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -28,3 +28,24 @@ test('Clicking checkbox enables button and clicking a second time disables butto
   await user.click(checkbox);
   expect(button).toBeDisabled();
 });
+
+test('popover starts out hidden', async () => {
+  render(<SummaryForm />);
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  await expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/);
+  expect(popover).toBeInTheDocument();
+  //popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  const nullPopoverAgain = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopoverAgain).not.toBeInTheDocument();
+});

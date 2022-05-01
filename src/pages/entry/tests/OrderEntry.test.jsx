c5aadbf9f0e15bf54d5566a4dacec61510e9a9b6
Carl Olson <colson2275@gmail.com>
2022-04-30 22:46:19 -0400

    update test

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -3,7 +3,7 @@ import OrderEntry from '../OrderEntry';
 import { rest } from 'msw';
 import { server } from '../../../mocks/server';
 
-test('handles error for scoops and toppings routes', async () => {
+test.only('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
     rest.get('http://localhost:3030/scoops', (req, res, ctx) =>
       res(ctx.status(500))
@@ -14,8 +14,11 @@ test('handles error for scoops and toppings routes', async () => {
   );
   render(<OrderEntry />);
 
-  const alerts = await screen.findAllByRole('alert', {
+  const alerts = await screen.findAllByRole('alert');
+
+  /*   const alerts = await screen.findAllByRole('alert', 
+  {
     name: 'An unexptected error occured. Please try again later.',
-  });
+  }); */
   expect(alerts).toHaveLength(2);
 });
